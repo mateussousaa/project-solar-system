ef37205bddaad13540d638ea032a0f704e7b9205
mateussousaa <mateussousa20@outlook.com>
2022-06-21 15:42:02 -0300

    requirement 04 - completed

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Title from './Title';
 
 class SolarSystem extends React.Component {
   render() {
-    return <div data-testid="solar-system" />;
+    return (
+      <div data-testid="solar-system">
+        <Title headline="Planetas" />
+      </div>
+    );
   }
 }
 
